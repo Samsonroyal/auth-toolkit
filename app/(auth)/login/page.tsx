7ce39dfb38daacd1ec3049967206af8d809f96e2
Were Samson Bruno <werefast2000@gmail.com>
2024-01-04 22:04:25 +0000

    created login route

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const LoginPage = () => {
+
+    return (
+        <div>
+        <h1>Login Page</h1>
+        <Button>Login</Button>
+        </div>
+    );
+    }
+
+    export default LoginPage;
